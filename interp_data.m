df080088f69fe10a988e4424de744bd3e1074139
Manuela Uliano <manuela.uliano@santannapisa.it>
2025-11-15 13:21:12 +0100

    moving things in organized folders

--- interp_data.m
+++
@@ -1,5 +0,0 @@
-function data_resampled = interp_data(data, len_max)
-        n_orig   = 1:size(data,1);     % unità di bin
-        n_target = linspace(1, size(data,1), len_max);        % unità di bin
-        data_resampled = round(interp1(n_orig, data, n_target, 'linear'));
-end 
