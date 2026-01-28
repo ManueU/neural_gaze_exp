bf36b326ed68a8e6b551fae1ca7c682a12757c3b
Manuela Uliano <manuela.uliano@santannapisa.it>
2026-01-28 11:41:11 +0100

    old version svm (balanced dataset)

---
+++ 02_Decoding/svm_cv_OLD.m
@@ -0,0 +1,19 @@
+function [acc, cm] = svm_cv_1(X, Y, k_fold)
+    cv = cvpartition(Y, 'KFold', k_fold);
+    Y_true = []; Y_pred = [];
+
+    t = templateSVM('KernelFunction','rbf','KernelScale','auto','Standardize',true);
+    for k = 1:k_fold
+        trainIdx = training(cv, k);
+        testIdx  = test(cv, k);
+
+        model = fitcecoc(X(trainIdx,:), Y(trainIdx), 'Learners', t, 'Coding', 'onevsall');
+        yhat  = predict(model, X(testIdx,:));
+
+        Y_true = [Y_true; Y(testIdx)];
+        Y_pred = [Y_pred; yhat];
+    end
+
+    acc = mean(Y_true == Y_pred);
+    cm  = confusionmat(Y_true, Y_pred);
+end
